223ecca571d25861ffa7ff9af5d85e4f7a03dd41
auraSyntax <aurasyntax@gmail.com>
2026-02-24 22:02:16 +0530

    Update AuthController.java

--- src/main/java/com/aura/syntax/ausempi/demo/api/controller/AuthController.java
+++ src/main/java/com/aura/syntax/ausempi/demo/api/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173","https://ausempi-web.vercel.app"})
+@CrossOrigin(origins = {"http://localhost:5173","https://ausempi-web.vercel.app", "https://ausempi.up.railway.app"})
 public class AuthController {
 
     private final AuthenticationService service;
